2bd556fd77be848e14f103926c386a818db71084
njoize <njoize@gmail.com>
2018-12-01 13:15:33 +0700

    Drawer and Hamberger Success

--- app/src/main/java/njoize/dai_ka/com/demotestprint/MyConstant.java
+++ app/src/main/java/njoize/dai_ka/com/demotestprint/MyConstant.java
@@ -5,6 +5,11 @@ public class MyConstant {
     //    Explicit
     private String urlGetUserWhereName = "http://www.brainwakecafe.com/android/getUserWhereName.php";
 
+    private String nameShopString = "Name Shop";
+
+    public String getNameShopString() {
+        return nameShopString;
+    }
 
     public String getUrlGetUserWhereName() {
         return urlGetUserWhereName;
--- app/src/main/java/njoize/dai_ka/com/demotestprint/ServiceActivity.java
+++ app/src/main/java/njoize/dai_ka/com/demotestprint/ServiceActivity.java
@@ -1,13 +1,113 @@
 package njoize.dai_ka.com.demotestprint;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //    Explicit
+    private MyConstant myConstant = new MyConstant();
+    private String loginJSONString, nameString;
+    private String tag = "1devV2";
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+//        Get ValueLogin
+        getValueLogin();
+
+
+//        Create Toolbar
+        createToolbar();
+
+    } // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
+
+
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    private void getValueLogin() {
+        loginJSONString = getIntent().getStringExtra("Login");
+        Log.d(tag, "JSON receive ==> " + loginJSONString);
+
+        try {
+
+            JSONArray jsonArray = new JSONArray(loginJSONString);
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            nameString = jsonObject.getString("name");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(myConstant.getNameShopString());
+        getSupportActionBar().setSubtitle("Login by : " + nameString);
+
+        drawerLayout = findViewById(R.id.layoutDrawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                ServiceActivity.this,
+                drawerLayout,
+                R.string.open,
+                R.string.close);
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_action_hamberger);
+
+
+
+    }
+} // Main Class
